2f18ee1644a21285babcf0d362adf17bc17cfdb1
Josh Donnell <joshua.donnell1@gmail.com>
2026-05-06 23:25:43 +0100

    chore: fix CI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace JoshDonnell\Radar\Tests;
 
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Schema;
 use JoshDonnell\Radar\Http\Middleware\Authorize;
 use Orchestra\Testbench\TestCase as Orchestra;
+use RuntimeException;
 
 class TestCase extends Orchestra
 {
@@ -21,9 +23,32 @@ class TestCase extends Orchestra
     {
         $app->make('config')->set('radar.middleware', [Authorize::class]);
 
+        $this->copyBuiltAssetsToPublicPath();
+
         Schema::dropAllTables();
 
         $migration = include __DIR__.'/../database/migrations/create_radar_scans_table.php.stub';
-        $migration->up();
+        $runMigration = [$migration, 'up'];
+
+        if (! is_callable($runMigration)) {
+            throw new RuntimeException('Radar scan migration stub must return an executable migration.');
+        }
+
+        $runMigration();
+    }
+
+    private function copyBuiltAssetsToPublicPath(): void
+    {
+        $filesystem = new Filesystem();
+        $sourcePath = __DIR__.'/../resources/dist';
+        $targetPath = public_path('vendor/radar');
+
+        $filesystem->deleteDirectory($targetPath);
+
+        if (! $filesystem->isDirectory($sourcePath)) {
+            return;
+        }
+
+        $filesystem->copyDirectory($sourcePath, $targetPath);
     }
 }
