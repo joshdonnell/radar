f8fd4d517b58e08511a6b2e344ec8e6f9ed90434
Josh Donnell <joshua.donnell1@gmail.com>
2026-05-06 19:30:31 +0100

    fix: radar config issue

--- src/Support/Config.php
+++ src/Support/Config.php
@@ -4,33 +4,10 @@ declare(strict_types=1);
 
 namespace JoshDonnell\Radar\Support;
 
-use JoshDonnell\Radar\Exceptions\InvalidConfig;
 use JoshDonnell\Radar\Http\Middleware\Authorize;
 
 final class Config
 {
-    /**
-     * @param  class-string  $defaultClass
-     * @return class-string
-     */
-    public static function getActionClass(string $actionName, string $defaultClass): string
-    {
-        $actionClass = config('radar.actions.'.$actionName, $defaultClass);
-
-        if (! is_string($actionClass) || ! class_exists($actionClass) || ! method_exists($actionClass, 'execute')) {
-            throw InvalidConfig::invalidAction($actionName, $actionClass);
-        }
-
-        return $actionClass;
-    }
-
-    public static function databaseConnection(): ?string
-    {
-        $connection = config('radar.storage.database.connection');
-
-        return is_string($connection) ? $connection : null;
-    }
-
     public static function authorizationGate(): string
     {
         $gate = config('radar.authorization.gate', 'viewRadar');
